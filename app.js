48fbeb8798907a76ce988c1de07a608c6909a134
tinymini <aberial@hotmail.com>
2018-01-07 23:36:33 +0900

    first init

---
+++ app.js
@@ -0,0 +1,597 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+// app.use("/ticker", function (req, res, next) {
+//   console.log('Request Type:', req.method);
+//   callAllTicker(function (data) {
+//     res.json(data);
+//   });
+// });
+// app.use("/market", function (req, res, next) {
+//   console.log('Request Type:', req.method);
+//   callMarket(function (data) {
+//     res.json(data);
+//   });
+// });
+// app.use("/getCurrency", function (req, res, next) {
+//   console.log("Get Currency:", req.method);
+//   callCurrency(function (data) {
+//     res.json(data);
+//   });
+// });
+
+//custom
+// var request = require('request');
+
+// var urls = {
+//   // bitthumb: "https://api.bithumb.com/public/ticker/all",
+//   // coinone: "https://api.coinone.co.kr/ticker?currency=all",
+//   // bittrex: "https://bittrex.com/api/v1.1/public/getmarketsummaries",
+//   // binance: "https://api.binance.com/api/v1/ticker/24hr",
+// }
+
+// var exchanges = {
+//   bitthumb: {
+//     url: "https://api.bithumb.com/public/ticker/all",
+//     tick: 3
+//   },
+//   coinone: {
+//     url: "https://api.coinone.co.kr/ticker?currency=all",
+//     tick: 6
+//   },
+//   bittrex: {
+//     url: "https://bittrex.com/api/v1.1/public/getmarketsummaries",
+//     tick: 9
+//   },
+//   binance: {
+//     url: "https://api.binance.com/api/v1/ticker/24hr",
+//     tick: 12
+//   },
+// }
+
+// var urls2 = {
+//   // bitfinex: {
+//   //   symbols: "https://api.bitfinex.com/v1/symbols",
+//   //   tickers: "https://api.bitfinex.com/v2/tickers?symbols=ç"
+//   // }
+// }
+
+// var currencyUrls = {
+//   yahoo: "https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json",
+// }
+// var ignoreProps = {
+//   coinone: ["errorCode", "result", "timestamp"],
+//   bitthumb: ["date"],
+// }
+// var constant = {
+//   day: 60 * 60 * 24,
+//   tick: 3
+// }
+// var bases = {
+//   krw: "KRW",
+//   jpy: "JPY",
+//   usd: "USD",
+//   btc: "BTC",
+// }
+// var baseValues = {
+//   qoute: { name: bases.krw, rate: 0 },
+//   currency: { name: bases.btc, rate: 0 }
+// }
+
+// init();
+// function init() {
+//   //환율 조회
+//   callCurrency(function (data) {
+//     if (data.length > 0) {
+//       qoutes = data;
+//     }
+//     console.log(data);
+//     var tempObj = _(qoutes).filter(function (data) {
+//       return data.name === baseValues.qoute.name;
+//     }).value();
+
+//     if (tempObj && tempObj.length > 0) {
+//       baseValues.qoute.rate = tempObj[0].price;
+//     }
+
+//     tempObj = _(qoutes).filter(function (data) {
+//       return data.name === baseValues.currency.name;
+//     }).value();
+//     if (tempObj && tempObj.length > 0) {
+//       baseValues.currency.rate = tempObj[0].exchange_rate;
+//     }
+//     console.log(baseValues);
+//   });
+
+//   // setInterval(function () {
+//   //   intervalCounter += constant.tick;
+//   //   callAllTicker(function (data) {
+//   //     console.log("memoize at : " + intervalCounter);
+//   //     memoizeList(data, intervalCounter);
+//   //   }, intervalCounter);
+//   // }, constant.tick * 1000)
+// }
+
+// var intervalCounter = 0;
+
+// // setInterval(callAllTicker, 1000);
+// function callCurrency(callback) {
+//   var resultArr = [];
+//   var keys = _.keys(currencyUrls);
+//   var cnt = 0;
+//   for (let i = 0, len = keys.length; i < len; i++) {
+//     requestCurrency(currencyUrls[keys[i]], keys[i], function (data) {
+//       resultArr = resultArr.concat(data);
+//       cnt++;
+//       console.log(cnt);
+//       if (cnt === keys.length) {
+//         callback(resultArr);
+//       }
+//     });
+//   }
+// }
+
+// function requestCurrency(url, source, callback) {
+//   var list = null;
+//   console.log(source);
+//   request({
+//     url: url,
+//     json: true
+//   }, function (error, response, body) {
+//     if (source == "tether") console.log(response.statusCode)
+//     if (error) {
+//       console.log(error);
+//     } else if (!error && response.statusCode === 200) {
+//       var data;
+//       switch (source) {
+//         case "yahoo":
+//           data = body.list.resources;
+//           list = _(data).map(function (value) {
+//             var resource = value && value.resource && value.resource.fields;
+//             var qouteName = resource.name.split("/")[1];
+//             if (qouteName && qouteName.length > 0) {
+//               resource.name = qouteName;
+//               return resource;
+//             } else {
+//               return null;
+//             }
+//           }).compact().value();
+//           break;
+//         case "tether":
+//           console.log(body);
+//           data = body;
+//           list = _(data).map(function (value) {
+//             if (value.source_currency.indexOf(bases.usd) > 0) {
+//               value.name = bases.btc;
+//               value.price = value.exchange_rate;
+//               return value;
+//             } else {
+//               return null;
+//             }
+//           }).compact().value();
+//           break;
+//       }
+//       if (callback) {
+//         callback(list);
+//       }
+//     }
+//   });
+// }
+
+// function callAllTicker(callback, counter) {
+//   var resultArr = [];
+//   var keys = _.keys(exchanges);
+//   var keys_len = keys.length;
+//   //다른 타입
+//   var keys2 = _.keys(urls2);
+//   var keys2_len = keys2.length;
+
+//   var total = keys_len + keys2_len;
+
+//   var cnt = 0;
+//   for (let i = 0, len = keys_len; i < len; i++) {
+//     var key = keys[i];
+//     if (counter === undefined) {
+//       requestTicker(exchanges[key].url, key, function (data) {
+//         resultArr = resultArr.concat(data);
+//         cnt++;
+//         if (cnt === total) {
+//           callback(resultArr);
+//         }
+//       });
+//     } else {
+//       if (counter % exchanges[key].tick === 0) {
+//         console.log(key, counter, exchanges[key].tick);
+//         requestTicker(exchanges[key].url, key, function (data) {
+//           resultArr = resultArr.concat(data);
+//           cnt++;
+//           if (cnt === total) {
+//             callback(resultArr);
+//           }
+//         });
+//       } else {
+//         total--;
+//       }
+//     }
+//   }
+
+//   for (let j = 0, jLen = keys2_len; j < jLen; j++) {
+//     requestTicker2(urls2[keys2[j]], keys2[j], function (data) {
+//       resultArr = resultArr.concat(data);
+//       console.log(keys2[i]);
+//       cnt++;
+//       if (cnt === total) {
+//         callback(resultArr);
+//       }
+//     })
+//   }
+// }
+
+// //bitfinex
+// function requestTicker2(urlObj, exchange, callback) {
+//   var list = null;
+//   request({
+//     url: urlObj.symbols,
+//     json: true
+//   }, function (error, response, body) {
+//     if (error) {
+//       console.log(error);
+//     } else if (!error && response.statusCode === 200) {
+//       var symbols = body;
+//       var param = _.map(symbols, function (even, idx, collection) {
+//         return "t" + even.toUpperCase()
+//       }).toString();
+//       request({
+//         url: urlObj.tickers + param,
+//         json: true
+//       }, function (error, response, body) {
+//         if (error) {
+//           console.log(error);
+//         } else if (!error && response.statusCode === 200) {
+//           var tickers = body;
+//           list = _(tickers).map(function (value) {
+//             return value ? tickerAdaptor(value, exchange) : null;
+//           }).value();
+//           if (callback) {
+//             callback(list);
+//           }
+//         }
+//       });
+//     }
+//   })
+// }
+
+// function requestTicker(url, exchange, callback) {
+//   request({
+//     url: url,
+//     json: true
+//   }, function (error, response, body) {
+//     if (error) {
+//       console.log(error);
+//     } else if (!error && response.statusCode === 200) {
+//       var data;
+//       switch (exchange) {
+//         case "bitthumb":
+//           data = body.data;
+//           break;
+//         case "bittrex":
+//           data = body.result;
+//           break;
+//         default:
+//           data = body;
+//           break;
+//       }
+//       var list = null;
+//       //array type
+//       if (_.isArray(data)) {
+//         list = _(data).map(function (value) {
+//           return value ? tickerAdaptor(value, exchange) : null;
+//         }).value();
+//         //object type
+//       } else if (_.isObject(data)) {
+//         var timestamp = data.timestamp || data.date;
+//         list = _(data).mapValues(function (value, id) {
+//           var obj = {
+//             currency: id.toUpperCase(),
+//             timestamp: timestamp
+//           }
+//           return _.isObject(value) ? _.merge(obj, value) : null;
+//         }).values().compact().map(function (value) {
+//           return value ? tickerAdaptor(value, exchange) : null;
+//         }).value();
+//       }
+//       if (callback) {
+//         callback(list);
+//       }
+//     }
+//   });
+// }
+// var memoize = [
+//   {
+//     max: 20,
+//     time: 3,
+//     list: [],
+//     type: "sec"
+//   },
+//   {
+//     max: 60,
+//     time: 60,
+//     list: [],
+//     type: "min"
+//   },
+//   {
+//     max: 24,
+//     time: 60 * 60,
+//     list: [],
+//     type: "hour"
+//   },
+//   {
+//     max: 30,
+//     time: 60 * 60 * 24,
+//     list: [],
+//     type: "day"
+//   },
+// ]
+// function memoizeList(list, tick) {
+//   var now = list;
+//   var past = null;
+
+//   for (let i = 0, len = memoize.length; i < len; i++) {
+//     var value = memoize[i];
+//     if (tick % value.time === 0) {
+//       if (value.list.length >= value.max) {
+//         value.list.shift();
+//       }
+//       if (i > 0) {
+//         past = memoize[i - 1].list;
+
+//       }
+//       value.list.push(now);
+//       console.log(value.type + " : " + value.list.length);
+//     }
+//     if (tick === constant.day) {
+//       tick = 0;
+//     }
+//   }
+// }
+
+// var qoutes = null;
+
+// var tickerArray = [];
+// var tickerVO = {
+//   first: null,
+//   last: null,
+//   high: null,
+//   low: null,
+//   volume: null,
+//   currency: null,
+//   exchange: null,
+//   market: null,
+//   custom: {},
+//   timestamp: null
+// };
+// var marketVO = {
+//   currency: null,
+//   base: null,
+//   currencyName: null,
+//   baseName: null,
+//   transUnit: null,
+//   isActive: null,
+//   exchange: null,
+//   market: null,
+//   custom: {}
+// };
+// var marketUrls = {
+//   bittrex: "https://bittrex.com/api/v1.1/public/getmarkets"
+// };
+// function callMarket(callback) {
+//   var resultArr = [];
+//   var keys = _.keys(marketUrls);
+//   var cnt = 0;
+//   for (let i = 0, len = keys.length; i < len; i++) {
+//     requestMarket(marketUrls[keys[i]], keys[i], function (data) {
+//       resultArr = resultArr.concat(data);
+//       cnt++;
+//       if (cnt === keys.length) {
+//         callback(resultArr);
+//       }
+//     });
+//   }
+// }
+// function requestMarket(url, exchange, callback) {
+//   request({
+//     url: url,
+//     json: true
+//   }, function (error, response, body) {
+//     if (error) {
+//       console.log(error);
+//     } else if (!error && response.statusCode === 200) {
+//       var data;
+//       switch (exchange) {
+//         case "bittrex":
+//           data = body.result;
+//           break;
+//         default:
+//           data = body;
+//           break;
+//       }
+//       var list = _(data).map(function (value) {
+//         return value ? marketAdaptor(value, exchange) : null;
+//       }).compact().value();
+
+//       if (callback) {
+//         callback(list);
+//       }
+//     }
+//   });
+// }
+
+// function marketAdaptor(data, exchange) {
+//   var obj = _.cloneDeep(marketVO);
+//   obj.exchange = exchange;
+//   switch (exchange) {
+//     case "bittrex":
+//       obj.currency = data.MarketCurrency;
+//       obj.base = data.BaseCurrency;
+//       obj.currencyName = data.MarketCurrencyLong;
+//       obj.baseName = data.BaseCurrencyLong;
+//       obj.transUnit = data.MinTradeSize;
+//       obj.isActive = data.isActive;
+//       obj.market = data.MarketName;
+//       obj.custom.created = data.Created;
+//       break;
+//   }
+//   return obj;
+// }
+
+// function currencyAdaptor(data) {
+//   return
+// }
+
+// function tickerAdaptor(data, exchange) {
+//   var obj = _.cloneDeep(tickerVO);
+//   obj.exchange = exchange;
+//   obj.timestamp = data.timestamp || data.TimeStamp;
+//   // 한국코인
+//   if (data.currency) {
+//     obj.currency = data.currency.toUpperCase();
+//     obj.base = bases.krw;
+//     obj.market = obj.base + "-" + obj.currency;
+//   } else {
+//     if (exchange === "bittrex") {
+//       obj.market = data.MarketName;
+//       var arr = obj.market.split("-");
+//       obj.currency = arr[1];
+//       obj.base = arr[0];
+//     } else if (exchange === "binance") {
+//       var symbol = data.symbol;
+//       obj.base = data.symbol.substring(symbol.length - 3);
+//       if (obj.base === "SDT") {
+//         obj.base = data.symbol.substring(symbol.length - 4);
+//         obj.currency = data.symbol.substring(0, symbol.length - 4);
+//       } else {
+//         obj.currency = data.symbol.substring(0, symbol.length - 3);
+//       }
+//       obj.market = obj.base + "-" + obj.currency;
+//     }
+//   }
+//   switch (exchange) {
+//     case "bitthumb":
+//       obj.first = data.opening_price;
+//       obj.last = data.closing_price;
+//       obj.high = data.max_price;
+//       obj.low = data.min_price;
+//       obj.volume = data.units_traded;
+//       obj.custom.avg = data.average_price;
+//       obj.custom.volume_1 = data.volume_1day;
+//       obj.custom.volume_7 = data.volume_7day;
+//       obj.custom.ask = data.buy_price;
+//       obj.custom.bid = data.sell_price;
+//       break;
+//     case "coinone":
+//       obj.first = data.first;
+//       obj.last = data.last;
+//       obj.high = data.high;
+//       obj.low = data.low;
+//       obj.volume = data.volume;
+//       obj.custom.yesterday_first = data.yesterday_first;
+//       obj.custom.yesterday_last = data.yesterday_last;
+//       obj.custom.yesterday_high = data.yesterday_high;
+//       obj.custom.yesterday_low = data.yesterday_low;
+//       break;
+//     case "bittrex":
+//       obj.first = data.PrevDay;
+//       obj.last = data.Last;
+//       obj.high = data.High;
+//       obj.low = data.Low;
+//       obj.volume = data.Volume;
+//       obj.custom.base_volume = data.BaseVolume;
+//       obj.custom.ask = data.Ask;
+//       obj.custom.bid = data.Bid;
+//       obj.custom.sell_orders = data.OpenSellOrders;
+//       obj.custom.buy_orders = data.OpenBuyOrders;
+//       break;
+//     case "binance":
+//       obj.first = data.openPrice;
+//       obj.last = data.lastPrice;
+//       obj.high = data.highPrice;
+//       obj.low = data.lowPrice;
+//       obj.volume = data.volume;
+//       obj.custom.price_change = data.priceChange;
+//       obj.custom.price_change_percent = data.priceChangePercent;
+//       obj.custom.base_volume = data.quoteVolume;
+//       obj.custom.last_qty = data.lastQty;
+//       obj.custom.ask = data.askPrice;
+//       obj.custom.ask_qty = data.askQty;
+//       obj.custom.bid = data.bidPrice;
+//       obj.custom.bid_qty = data.bidQty;
+//       obj.custom.avg = data.weightedAvgPrice;
+//       obj.custom.yesterday_last = data.prevClosePrice;
+//       obj.custom.trade_count = data.count;
+//       break;
+//     case "bitfinex":
+//       obj.first = +parseFloat(data[7] - data[5]).toFixed(8);
+//       obj.last = data[7];
+//       obj.high = data[9];
+//       obj.low = data[10];
+//       obj.volume = data[8];
+//       obj.custom.price_change = data[5];
+//       obj.custom.price_change_percent = data[6];
+//       obj.custom.ask = data[3];
+//       obj.custom.ask_qty = data[4];
+//       obj.custom.bid = data[1];
+//       obj.custom.bid_qty = data[2];
+//       var symbol = data[0].substring(1);
+//       obj.base = symbol.substring(symbol.length - 3);
+//       obj.currency = symbol.substring(0, symbol.length - 3);
+//       obj.market = obj.base + "-" + obj.currency;
+//       break;
+//   }
+//   return obj;
+// }
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
---
+++ public/javascripts/script.js
@@ -0,0 +1,96 @@
+// var count = 100;
+// var itvLimit = 20;
+// var all = [];
+// itvExec();
+
+// function itvExec() {
+//     var itvCount = 0;
+//     var itv = setInterval(function () {
+//         all.push(makeArr(count));
+//         itvCount++;
+//         if (itvLimit === itvCount) {
+//             clearInterval(itv);
+//             callback();
+//         }
+//     }, 0);
+// }
+// var trunk = {};
+
+// function callback() {
+//     _(all).map(function (value, index, collenction) {
+//         _.forEach(value, function (value) {
+//             var id = value.id;
+//             var name = value.name;
+
+//             if (trunk[id + name] === undefined) {
+//                 trunk[id + name] = [];
+//             }
+//             trunk[id + name].push(value);
+//         });
+//     }).value();
+//     statstics(trunk);
+// }
+
+// function statstics(trunk) {
+//     var stts = {};
+//     var keys = _.keys(trunk);
+
+//     var arr = _(trunk).forEach(function (value, key, collenction) {
+//         var first = value[0], last = value[value.length - 1];
+//         var obj = {
+//             high: _.maxBy(value, "high").high,
+//             low: _.minBy(value, "low").low,
+//             first: first.first,
+//             last: last.last,
+//             volume: last.volume,
+//             volumeIncrease: last.volume - first.volume,
+//         };
+//         stts[key] = obj;
+//     });
+//     console.log(stts);
+// }
+
+
+
+// function makeArr(count) {
+//     var arr = [];
+//     for (var i = 0; i < count; i++) {
+//         var obj = {
+//             id: "id_" + i % 10,
+//             name: "name_" + i % 20,
+//             high: Math.floor(Math.random() * 10000) + 1,
+//             low: Math.floor(Math.random() * 10000) + 1,
+//             first: Math.floor(Math.random() * 10000) + 1,
+//             last: Math.floor(Math.random() * 10000) + 1,
+//             volume: Math.floor(Math.random() * 10000) + 1,
+//             dummy: {
+//                 trash: Math.floor(Math.random() * 10000) + 1,
+//             }
+//         }
+//         arr.push(obj);
+//     }
+//     return arr;
+// }
+
+var socket = io.connect('http://localhost:3001');
+socket.on('data', function (data) {
+    var listDiv = document.getElementById("list");
+
+    var str = "";
+    _.forEach(data, function (value, key) {
+        _.forEach(value.list, function(_value, _key){
+            console.log(_value, _key);
+        });
+    });
+    list.innerHTML = str;
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var clickBtn = document.getElementById("click");
+    clickBtn.addEventListener("click", function (e) {
+        socket.emit("call");
+    });
+});
+
+function makeDiv(even, key) {
+    return "<div>" + key + " // high : " + even.high + ", low : " + even.low + ", first : " + even.first + ", last :" + even.last + ", volume : " + even.volume + "</div>";
+}
